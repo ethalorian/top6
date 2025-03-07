962c01d9c7631c8cf805124f3681444679e7a241
ethalorian <80971826+ethalorian@users.noreply.github.com>
2025-03-06 23:13:32 -0500

    test

--- src/components/ContentPanel.tsx
+++ src/components/ContentPanel.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export function ContentPanel() {
   return (
     <div className="space-y-[5%] w-full h-full flex flex-col justify-center">
-      <div className="w-[80%] mb-[4%]">
+      <div className="w-[80%] mb-[4%] mx-auto">
         <div className="relative w-full aspect-[1/1]">
           <Image 
             src="/top6-logo.svg" 
@@ -23,8 +23,8 @@ export function ContentPanel() {
           Curate your Top 6 Universal Profiles, all in one grid for your followers to discover and connect with new UPs.
         </p>
 
-        <div className="mt-[5%]">
-          <Button className="bg-[#0f172a] hover:bg-[#1e293b] text-white py-[3%] px-[6%] rounded-sm text-[clamp(0.9rem,1.8vw,1.3rem)] h-auto w-[40%]">
+        <div className="mt-[5%] flex justify-center">
+          <Button className="bg-[#0f172a] hover:bg-[#1e293b] text-white py-[3%] px-[6%] rounded-sm text-[clamp(0.9rem,1.8vw,1.3rem)] h-auto w-[100%]">
             Follow TOP 6
           </Button>
         </div>
