6acb327564d8b68efaa3b6af5b2080bed68f200b
ethalorian <80971826+ethalorian@users.noreply.github.com>
2025-03-06 21:46:40 -0500

    test

--- src/components/ContentPanel.tsx
+++ src/components/ContentPanel.tsx
@@ -4,8 +4,8 @@ import Image from 'next/image'
 export function ContentPanel() {
   return (
     <div className="space-y-[5%] w-full h-full flex flex-col justify-center">
-      <div className="w-[90%] mb-[4%]">
-        <div className="relative w-full aspect-[8/3] transform scale-[2.5]">
+      <div className="w-[80%] mb-[4%]">
+        <div className="relative w-full aspect-[8/3]">
           <Image 
             src="/top6-logo.svg" 
             alt="TOP 6" 
